a8caf7b58487e6d51edb9f0a1976c99ca23cdf3c
Carlos Larios <20460440@colima.tecnm.mx>
2023-07-10 00:59:50 -0600

    Prueba con Thunder Client

--- node-Back/database/db.js
+++ node-Back/database/db.js
@@ -1,9 +1,10 @@
+
 import { Sequelize } from "sequelize";
 
 //La 3ra es la contraseña
 const db = new Sequelize('database_app','root','',{
     host:'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
 })
 
 export default db
